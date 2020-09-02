7d1866a4fa2dc5efff2cbb760c57752b7f8deb7c
assaf970se <assafse@wix.com>
2020-09-02 17:09:07 +0300

    dynamic lists

---
+++ resolve/js/app.js
@@ -0,0 +1,134 @@
+(function () {
+    const data = {
+        keynote: [
+            {
+                name: "Harry Moseley",
+                imgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_5c7d6e5eca174384b1c27f01d2ba69e3~mv2.jpg",
+                socialImgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.png/v1/crop/x_92,y_92,w_2032,h_2032/fill/w_22,h_22,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.webp",
+                socialLink: "https://www.linkedin.com/in/harrymoseley/",
+                title: "CIO",
+                logoUrl:
+                    "https://static.wixstatic.com/media/3cac9b_abab368cf57549a5943599023454fc86~mv2.png/v1/fill/w_66,h_30,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_abab368cf57549a5943599023454fc86~mv2.webp",
+            },
+            {
+                name: "Harry Moseley",
+                imgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_5c7d6e5eca174384b1c27f01d2ba69e3~mv2.jpg",
+                socialImgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.png/v1/crop/x_92,y_92,w_2032,h_2032/fill/w_22,h_22,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.webp",
+                socialLink: "https://www.linkedin.com/in/harrymoseley/",
+                title: "CIO",
+                logoUrl:
+                    "https://static.wixstatic.com/media/3cac9b_abab368cf57549a5943599023454fc86~mv2.png/v1/fill/w_66,h_30,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_abab368cf57549a5943599023454fc86~mv2.webp",
+            },
+        ],
+        speakers: [
+            {
+                name: "Harry Moseley",
+                imgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_5c7d6e5eca174384b1c27f01d2ba69e3~mv2.jpg",
+                socialImgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.png/v1/crop/x_92,y_92,w_2032,h_2032/fill/w_22,h_22,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.webp",
+                socialLink: "https://www.linkedin.com/in/harrymoseley/",
+                title: "CIO",
+                logoUrl:
+                    "https://static.wixstatic.com/media/3cac9b_abab368cf57549a5943599023454fc86~mv2.png/v1/fill/w_66,h_30,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_abab368cf57549a5943599023454fc86~mv2.webp",
+            },
+            {
+                name: "Harry Moseley",
+                imgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_5c7d6e5eca174384b1c27f01d2ba69e3~mv2.jpg",
+                socialImgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.png/v1/crop/x_92,y_92,w_2032,h_2032/fill/w_22,h_22,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.webp",
+                socialLink: "https://www.linkedin.com/in/harrymoseley/",
+                title: "CIO",
+                logoUrl:
+                    "https://static.wixstatic.com/media/3cac9b_abab368cf57549a5943599023454fc86~mv2.png/v1/fill/w_66,h_30,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_abab368cf57549a5943599023454fc86~mv2.webp",
+            },
+            {
+                name: "Harry Moseley",
+                imgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_5c7d6e5eca174384b1c27f01d2ba69e3~mv2.jpg",
+                socialImgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.png/v1/crop/x_92,y_92,w_2032,h_2032/fill/w_22,h_22,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.webp",
+                socialLink: "https://www.linkedin.com/in/harrymoseley/",
+                title: "CIO",
+                logoUrl:
+                    "https://static.wixstatic.com/media/3cac9b_abab368cf57549a5943599023454fc86~mv2.png/v1/fill/w_66,h_30,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_abab368cf57549a5943599023454fc86~mv2.webp",
+            },
+            {
+                name: "Harry Moseley",
+                imgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_5c7d6e5eca174384b1c27f01d2ba69e3~mv2.jpg",
+                socialImgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.png/v1/crop/x_92,y_92,w_2032,h_2032/fill/w_22,h_22,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.webp",
+                socialLink: "https://www.linkedin.com/in/harrymoseley/",
+                title: "CIO",
+                logoUrl:
+                    "https://static.wixstatic.com/media/3cac9b_abab368cf57549a5943599023454fc86~mv2.png/v1/fill/w_66,h_30,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_abab368cf57549a5943599023454fc86~mv2.webp",
+            },
+            {
+                name: "Harry Moseley",
+                imgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_5c7d6e5eca174384b1c27f01d2ba69e3~mv2.jpg",
+                socialImgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.png/v1/crop/x_92,y_92,w_2032,h_2032/fill/w_22,h_22,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.webp",
+                socialLink: "https://www.linkedin.com/in/harrymoseley/",
+                title: "CIO",
+                logoUrl:
+                    "https://static.wixstatic.com/media/3cac9b_abab368cf57549a5943599023454fc86~mv2.png/v1/fill/w_66,h_30,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_abab368cf57549a5943599023454fc86~mv2.webp",
+            },
+            {
+                name: "Harry Moseley",
+                imgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_5c7d6e5eca174384b1c27f01d2ba69e3~mv2.jpg",
+                socialImgUrl:
+                    "https://static.wixstatic.com/media/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.png/v1/crop/x_92,y_92,w_2032,h_2032/fill/w_22,h_22,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_c2b6138b6ed94063b7c0e1648255c096~mv2.webp",
+                socialLink: "https://www.linkedin.com/in/harrymoseley/",
+                title: "CIO",
+                logoUrl:
+                    "https://static.wixstatic.com/media/3cac9b_abab368cf57549a5943599023454fc86~mv2.png/v1/fill/w_66,h_30,al_c,q_85,usm_0.66_1.00_0.01/3cac9b_abab368cf57549a5943599023454fc86~mv2.webp",
+            },
+        ],
+    };
+
+    function renderSpeaker(speakerData){
+        return `
+        <div class="speaker">
+        <div class="speaker-img">
+            <img src=${speakerData.imgUrl} alt="">
+        </div>
+        <div class="speaker-details">
+            <div class="speaker-name">${speakerData.name}</div>
+            <div class="speaker-link">
+                <a href=${speakerData.socialLink}>
+                <img src=${speakerData.socialImgUrl} alt="">
+                </a>
+            </div>
+        </div>
+        <div class="speaker-work">
+            <div class="speaker-title">${speakerData.title} at</div>
+            <div class="speaker-company">
+                <img src=${speakerData.logoUrl} alt="">
+            </div>
+        </div>
+    </div>
+        `;
+    }
+
+    function renderKeynote() {
+        const keynoteHtml = data.keynote.map(renderSpeaker).join("")
+        document.querySelector('.keynote').innerHTML = keynoteHtml;
+    }
+
+    function renderSpeakers() {
+        const speakersHtml = data.speakers.map(renderSpeaker).join("")
+        document.querySelector('.speakers').innerHTML = speakersHtml;
+    }
+    
+    if(data.keynote) renderKeynote();
+    if(data.speakers) renderSpeakers();
+
+})();
